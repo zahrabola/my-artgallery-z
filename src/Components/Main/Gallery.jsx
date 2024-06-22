fb45bed077568527aa166a02668d3a1745f8227d
zahrabola <95353397+zahrabola@users.noreply.github.com>
2024-06-22 11:36:19 +0100

    update

--- src/Components/Main/Gallery.jsx
+++ src/Components/Main/Gallery.jsx
@@ -1,6 +1,6 @@
 import SearchResult from "./SearchResult";
 import Sidebar from "./Sidebar";
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 //https://github.com/luthvirtue/react-airlines-test/blob/master/src/components/SearchBar.js
 //https://api.artic.edu/api/v1/artworks/search?q=${search}&fields=id,title,image_id,artist_title&limit=30
@@ -10,17 +10,105 @@ import React from "react";
 
 const Gallery = () => {
 
+  const searchTerms = [
+    "Modern",
+    "Picasso",
+    "Contemporary",
+    "Oil Painting",
+    "Sculpture",
+    "Impressionism",
+    "Abstract",
+    "Palette",
+    "Canvas",
+    "Watercolor",
+    "Expressionism",
+    "Palette Knife",
+    "Cubism",
+    "Easel",
+    "Fresco",
+    "Pop Art",
+    "Graffiti",
+    "Dadaism",
+    "Surrealism",
+    "Engraving",
+    "Ink",
+    "Chiaroscuro",
+    "Monet",
+    "Portraiture",
+    "Gouache",
+    "Calligraphy",
+    "Art Nouveau",
+    "Fauvism",
+    "Pointillism",
+    "Collage",
+    "Renaissance",
+    "Realism",
+    "Still Life",
+    "Bronze",
+    "Marble",
+    "Landscape",
+    "Carving",
+    "Mosaic",
+    "Mural",
+    "Van Gogh",
+    "Abstract Expressionism",
+    "Printmaking",
+    "Pastel",
+    "Sgraffito",
+    "Installation",
+    "Glaze",
+    "Hue",
+    "Mondrian",
+    "Ceramics",
+    "Varnish",
+  ];
 
+  const [search, setSearch] = useState(searchTerms[0]); // Set initial search term
+  const [data, setData] = useState([]);
+  const [error, setError] = useState(null);
+
+
+
+  function getResults(search) {
+    const url = `https://api.artic.edu/api/v1/artworks/search?q=${search}&fields=id,title,image_id,artist_title&limit=30`;
+  
+    fetch(url)
+      .then((response) => response.json()) 
+      .then((response) => {
+        // Handle successful response
+        setData(response.data); 
+      })
+      .catch((error) => {
+        // Handle errors
+        setError(error); 
+      });
+  }
+  useEffect(() => {
+    getResults(search); 
+  }, [search]);
+
+
+  function handleChange(event) {
+    setSearch(event.target.value);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    getResults(search);
+  }
 
   return (
     <div>
       gallery
-     
+      {error && <p>Error: {error.message}</p>}
       <Sidebar
+      handleChange={handleChange}
+      handleSubmit={handleSubmit}
       
        
       />
-      <SearchResult  />
+      <SearchResult data={data} />
     </div>
   );
 };

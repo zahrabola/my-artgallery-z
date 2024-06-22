d2642a4123fb12b412dce3e20e80a181b5b0edcb
zahrabola <95353397+zahrabola@users.noreply.github.com>
2024-06-22 12:11:50 +0100

    update

--- src/Components/Main/CardResults.jsx
+++ src/Components/Main/CardResults.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./main.css"
+import "./main.css";
 const CardResults = ({ artitem }) => {
   return (
     <div className="card">
@@ -7,7 +7,9 @@ const CardResults = ({ artitem }) => {
         src={`https://www.artic.edu/iiif/2/${artitem.image_id}/full/843,/0/default.jpg`}
         alt="imagephoto"
       ></img>
-{  /*    {artitem.title} - {artitem.artist_title} - {artitem.date_display} */}
+      <div className="card-overlay">
+        {artitem.title} - {artitem.artist_title} - {artitem.date_display}
+      </div>
     </div>
   );
 };
